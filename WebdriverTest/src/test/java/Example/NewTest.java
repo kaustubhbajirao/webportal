e3e11aa1532914bf6520486c3ec051fce015b611
kaustubhbajirao <kaustubhbajirao@gmail.com>
2023-06-02 23:22:45 +0530

    Commiting data in repo

---
+++ WebdriverTest/src/test/java/Example/NewTest.java
@@ -0,0 +1,28 @@
+package Example;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		 System.setProperty("webdriver.firefox.marionette","C://Users//Kaustubh//eclipse//java-2022-122//eclipse//geckodriver.exe");
+	    driver = new FirefoxDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+}	
